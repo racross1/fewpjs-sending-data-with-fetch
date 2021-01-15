9d8a03f17edf83f8131456f45d95ce567613d833
racross1 <racross1@gmail.com>
2021-01-14 19:50:08 -0800

    commit tree 082ce0f3c55bb4b5c8a593d0f8d15a2de279397a

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// let configObj = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
+
+let dataObj = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+    function submitData(name, email){
+        return fetch("http://localhost:3000/users", dataObj)
+            .then(resp => resp.json())
+            .then(data => data.id)
+    
+    }
+
+    // return fetch(imgUrl)
+    //     .then((resp) => resp.json())
+    //     .then((dogPix) => {
+    //       dogPix["message"].forEach((dogPic) => insertImage(dogPic));
+    //     });
+    // }
